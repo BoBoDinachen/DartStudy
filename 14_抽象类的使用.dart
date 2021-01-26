e0582008aea2e9019eee641f37ebc20217836faa
XDEcat <492697494@qq.com>
2021-01-26 16:29:55 +0800

    抽象类的使用

---
+++ 14_抽象类的使用.dart
@@ -0,0 +1,51 @@
+import '12_常量构造函数.dart';
+
+main(List<String> args) {
+  // final p = new Person();
+  final r = new Recangle(10, 20);
+  final c = new Circle(2);
+  calculArea(r);
+  calculArea(c);
+}
+
+/**
+ * 1.抽象类不能被实例化
+ * 2.抽象类中可以定义抽象 
+ * 3.普通的类中不能定义抽象方法
+ * 4.如果一个普通类继承了抽象类，那么抽象类中的方法必须要被实现
+ */
+abstract class Person {
+  late String name;
+  late int age;
+}
+
+abstract class Shape {
+  double getArea();
+}
+
+class Recangle extends Shape {
+  late double width;
+  late double height;
+  Recangle(this.width, this.height);
+  @override
+  double getArea() {
+    // TODO: implement getArea
+    return this.width * this.height;
+  }
+}
+
+class Circle extends Shape {
+  late int r;
+  final PI = 3.14;
+  Circle(this.r);
+  @override
+  double getArea() {
+    // TODO: implement getArea
+    return PI * r * r;
+  }
+}
+
+// 计算面积
+void calculArea(Shape s) {
+  print("面积：${s.getArea()}");
+}
